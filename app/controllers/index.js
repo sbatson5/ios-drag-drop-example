92279437fb0968001e07d631344e74753a14f9ca
Scott Batson <sjbats@email.com>
2018-05-04 11:24:24 -0400

    drag-and-drop

---
+++ app/controllers/index.js
@@ -0,0 +1,25 @@
+import Controller from '@ember/controller';
+
+export default Controller.extend({
+  useSwap: true,
+  sortFinishText: null,
+  init() {
+    this._super(...arguments);
+    this.set('sortableObjectList', [
+      {id: 1, title:'Number 1'},
+      {id: 2, title:'Number 2'},
+      {id: 3, title:'Number 3'},
+      {id: 4, title:'Number 4'},
+      {id: 5, title:'Number 5'},
+      {id: 6, title:'Number 6'},
+      {id: 7, title:'Number 7'},
+      {id: 8, title:'Number 8'}
+    ]);
+  },
+
+  actions: {
+    sortEndAction() {
+      console.log('Sort Ended', this.get('sortableObjectList'));
+    }
+  }
+});
